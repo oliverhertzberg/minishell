6d76e5cc18807ca8a11158807f58178050d65bb5
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-17 18:27:08 +0300

    Update builtins.h

--- headers/builtins.h
+++ headers/builtins.h
@@ -4,7 +4,16 @@
 # include <unistd.h>
 # include <stdio.h>
 
+# include "utils.h"
+# include "../libft/libft.h"
+
+typedef struct s_var
+{
+	char	*export_var;
+	char	*export_string;
+}	t_var;
+
 void	ft_pwd(void);
-void	ft_echo(char *input);
+void	ft_echo(char *input)
 
 #endif
