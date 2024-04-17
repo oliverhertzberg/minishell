b950c77830a8ca55ab3721abee53bb2aacd9a793
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-17 15:49:54 +0300

    Update main.c

--- main.c
+++ main.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv, char **env)
 		input = readline("Minisell:$ ");
 		if (ft_strcmp(input, "pwd") == 0)
 			ft_pwd();
+		else if (ft_strncmp(input, "echo", 4) == 0)
+			ft_echo(input);
 		else
 			printf("%s\n", input);
 		add_history(input);
