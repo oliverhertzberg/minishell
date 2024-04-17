0ce7424fb87b3b534b1b7448d9b522160f2a2425
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-17 18:57:00 +0300

    Update main.c

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ int main(int argc, char **argv, char **env)
 	while (1)
 	{
 		input = readline("Minishell:$ ");
+		ft_strip(&input);
 		if (ft_strcmp(input, "pwd") == 0)
 			ft_pwd();
 		else if (ft_strncmp(input, "echo", 4) == 0)
