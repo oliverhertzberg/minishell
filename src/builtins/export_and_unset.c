06317d4a285e3c0d3d92405eb9c733ef8b590dc4
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-18 13:25:45 +0300

    Add files via upload

---
+++ src/builtins/export_and_unset.c
@@ -0,0 +1,21 @@
+#include "../../headers/builtins.h"
+
+/* using those functions from var_utils to actually make export work 
+ * properly. take key and value from input and put it inside of some strings,
+ * then check if key exists, if it does, if u have value, free old one and 
+ * save new one using change_value, if u have only key, don't do anything.
+ * if key doesn't exist, add it inside of v.
+ * */
+void	ft_export(char *input, t_var **v)
+{
+
+}
+
+/* it needs to work for: unset  key.
+ * u need to skip unset and all white spaces after it, and save key
+ * into some string. then check if u have that key in v, and if u do
+ * just remove it using function remove_var, if not do nothing */
+void	ft_unset(char *input, t_var **v)
+{
+
+}
