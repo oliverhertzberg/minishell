14f977781eb7d1567dabeedf36be1d6a7c41672c
Simos Tigkas <stigkas@c2r1p7.hive.fi>
2024-04-18 14:15:53 +0300

    added to var_utils

--- src/builtins/var_utils.c
+++ src/builtins/var_utils.c
@@ -33,12 +33,45 @@ int	key_exists(t_var *v, char *key)
 /* like adding at the back of the list node with key and value */
 void	add_new_var(t_var **v, char *key, char *value)
 {
+	t_var *node;
+	t_var *temp;
 
+	if (!key || !value)
+		return ;
+	node->key = key; //(*node).key
+	node->value = value;
+	node->next = NULL;
+	if (*v == NULL)
+		*v = node;
+	else
+	{
+		temp = *v;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = *node;
+	}
 }
 
 /* remove node from list v, that has concrete key in it */
 void	remove_var(t_var **v, char *key)
 {
+	t_var	*node;
+	t_var	*temp;
+
+	node = *v;
+	while (node->next)
+	{
+		if (node->next->key == key)
+		{
+			temp = node->next;
+			node = node->next->next;
+			free(temp->key);
+			free(temp->value);
+			temp->next = NULL //might not be needed
+			break ;
+		}
+		node = node->next;
+	}
 }
 
 /* if u find key inside of list v, free it's value and allocate new string with value that we are sending as an arg */
