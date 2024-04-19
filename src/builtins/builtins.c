47118426afc6338858c7fe89d6ed900d13d238e7
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-19 10:39:43 +0300

    Create builtins.c

---
+++ src/builtins/builtins.c
@@ -0,0 +1,10 @@
+#include "../../headers/builtins.h"
+
+t_builtins  do_builtins(char **line) //sending pointer to a string so that we can change it in the memory
+{
+  t_builtins  b;
+  
+  ft_strip(line);
+  
+  return (b);
+}
