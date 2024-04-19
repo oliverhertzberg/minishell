55046b7211facc233665cb7009fe11c0f5fb1cdc
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-19 10:30:18 +0300

    Update builtins.h

--- headers/builtins.h
+++ headers/builtins.h
@@ -18,6 +18,12 @@ typedef struct s_var
 	struct s_var	*next;
 }	t_var;
 
+typedef struct s_builtins
+{
+	char	*name; //name of builtins that we are using
+	t_var	*v;
+}	t_builtins;
+
 int	key_exists(t_var *v, char *key);
 char	*take_key(char *input);
 char	*take_value(char *input);
