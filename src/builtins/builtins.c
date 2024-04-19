e879f2b35977f3a9e3f89149c1b4c4bfc6dca63f
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-19 10:45:17 +0300

    Update builtins.c

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -1,10 +1,24 @@
 #include "../../headers/builtins.h"
 
-t_builtins  do_builtins(char **line) //sending pointer to a string so that we can change it in the memory
+t_builtins  create_builtins(char **line) //sending pointer to a string so that we can change it in the memory
 {
   t_builtins  b;
+  int  i;
   
   ft_strip(line);
-  
+  if (!line)
+    return (NULL);
+  i = 0;
+  while (*line[i] != 0 && ft_isspace(*line[i]) == 0)
+    i++;
+  b.name = malloc(i + 1);
+  i = 0;
+  //taking builtins name and putting into struct
+  while (*line[i] != 0 && ft_isspace(*line[i]) == 0)
+    {
+      b.name[i] = *line[i];
+      i++;
+    }
+  do_builtins(); //create this based on different builtins what should happen
   return (b);
 }
