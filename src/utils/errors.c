e1aec200e2e1d40a3925fb43dde9dc9876fca66f
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-24 18:18:06 +0300

    Update errors.c

--- src/utils/errors.c
+++ src/utils/errors.c
@@ -1,3 +1,5 @@
+#include "../../headers/utils.h"
+
 void	error_msg(char *c, char *str)
 {
 	write(2, "pipex: ", 7);
@@ -5,6 +7,12 @@ void	error_msg(char *c, char *str)
 		write(2, c, ft_strlen(c));
 	if (str)
 		write(2, str, ft_strlen(str));
+}
+
+void	malloc_error(void)
+{
+	write(2, "pipex:  alloaction failed\n", 26);
+	exit(1);
 }
 
 
