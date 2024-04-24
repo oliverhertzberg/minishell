deb939238177fcdcac384b2d10a4140ef7dc9bef
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-24 12:25:30 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,5 @@
 #include "../headers/minishell.h"
 
-/*void	get_env(t_var *hashmap, char **env)
-{
-	if (!env)
-		return ;
-	ft_export();
-	
-}*/
-
 int main(int argc, char **argv, char **env)
 {
 	char	*input;
@@ -16,19 +8,17 @@ int main(int argc, char **argv, char **env)
 	(void)argc;
 	(void)argv;
 	
-	//get_env(env, &hashmap);
+	//filling hashmap with env values and checking if env is emptly!
 	while (1)
 	{
 		input = readline("Minishell:$ ");
 		ft_strip(&input);
-		if (ft_strcmp(input, "pwd") == 0)
-			ft_pwd();
-		else if (ft_strncmp(input, "echo", 4) == 0)
-			ft_echo(input);
-		else if (ft_strcmp(input, "env") == 0)
-			ft_env(&env);
-		else
-			printf("%s\n", input);
+		// split it into list of struct
+		// cleaning strings based on quotes and spaces
+		// taking informations or printing errors if needed and freeing everything
+		// using pipex or builtings or both :) 
+		// free everything
+		
 		add_history(input);
 		free(input);
 	}
