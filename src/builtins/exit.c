7f9b58c7cbc0f5d4cc811c75ee1cd3181f01cfa0
Safa Tuna <56443802+tunasafa@users.noreply.github.com>
2024-04-25 13:59:41 +0300

    Update exit.c

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -1,6 +1,20 @@
-void ft_exit(char *input)
+static void free_input(char **input)
+{
+  int i;
+
+  i = 0;
+  while (input[i] != NULL)
+  {
+    free(input[i]);
+    i++;
+  }
+  free(input);
+}
+
+void ft_exit(char **input)
 {
   free(input);
   ft_putstr_fd("Exiting minishell...\n", STDERR_FILENO);
+  
   exit(0);
 }
