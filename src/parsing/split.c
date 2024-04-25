4330adaf9d83b589c9841cb2c196687e2128b869
Oliver Hertzberg <ohertzbe@c2r6p12.hive.fi>
2024-04-25 11:53:03 +0300

    use strlcpy instead of writing the function again for no reason

--- src/parsing/split.c
+++ src/parsing/split.c
@@ -16,18 +16,14 @@ static void in_quote(char *input, int *i, char *quote, t_parser **p)
 static void    create_node(t_parser *new, char *input, int start, int end)
 {
     char *string;
-    int i;
 
     string = (char *)malloc(sizeof(char) * (end - start + 1));
     if (!string)
         // free everything
         malloc_error();
-    i = start;
-    while (i < end)
-    {
-        *string = input[i++];
-        string++;
-    }
+    ft_strlcpy(string, input + start, end - start + 1);
+    if (string == NULL)
+        malloc_error();
     *string = '\0';
     new = lstnew(string);
     free(string);
