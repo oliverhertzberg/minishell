6176258173ac35255c081f1f89264834b428ff16
Simos Tigkas <stigkas@c2r6p11.hive.fi>
2024-04-26 14:46:25 +0300

    finished unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -1,11 +1,10 @@
 #include "../../headers/builtins.h"
 
-static int	unset_error(char *arg)
+static void	unset_error(char *input)
 {
-	ft_putstr_fd("minishell 🐢: unset: `", 2);
-	ft_putstr_fd(arg, 2);
+	ft_putstr_fd("Minishell: unset: '", 2);
+	ft_putstr_fd(input, 2);
 	ft_putstr_fd("': not a valid identifier\n", 2);
-	return (0);
 }
 
 /* it needs to work for: unset  key.
@@ -14,9 +13,13 @@ static int	unset_error(char *arg)
  * just remove it using function remove_var, if not do nothing */
 void	ft_unset(char *input, t_hmap **v)
 {
-	while (ft_isspace(input[*i]) == 1)
-    *i++;
-//   if (input[*i] == 0)
-//   //error
+	char *key_to_unset;
 
+	if (!input)
+		return ;
+	key_to_unset = take_key(input, 0);
+	if (key_exists(key_to_unset) == 1)
+		remove_var(v, key_to_unset);
+	else
+		unset_error(input);
 }
