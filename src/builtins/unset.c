5095fa8be69daef3ec57367b87e5d6a90eb1019e
Simos Tigkas <stigkas@c2r5p11.hive.fi>
2024-04-29 09:58:06 +0300

    fixed unset()

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -1,12 +1,5 @@
 #include "../../headers/builtins.h"
 
-static void	unset_error(char *input)
-{
-	ft_putstr_fd("Minishell: unset: '", 2);
-	ft_putstr_fd(input, 2);
-	ft_putstr_fd("': not a valid identifier\n", 2);
-}
-
 /* it needs to work for: unset  key.
  * u need to skip unset and all white spaces after it, and save key
  * into some string. then check if u have that key in v, and if u do
@@ -20,6 +13,4 @@ void	ft_unset(char *input, t_hmap **v)
 	key_to_unset = take_key(input, 0);
 	if (key_exists(key_to_unset) == 1)
 		remove_var(v, key_to_unset);
-	else
-		unset_error(input);
 }
