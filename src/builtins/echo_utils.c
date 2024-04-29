b8e55a17644ff774c50a3dc818d30108b53479f4
TanjaMenkovic <85727359+TanjaMenkovic@users.noreply.github.com>
2024-04-29 14:57:33 +0300

    Update echo_utils.c

--- src/builtins/echo_utils.c
+++ src/builtins/echo_utils.c
@@ -1,10 +1,24 @@
 #include "../../headers/builtins.h"
 
-void	print_space(char *input, int i)
+void	print_space(t_builtins *b, int i)
 {
-	if (input[i] != 0)
+	if (b->value[i] != 0)
 	{
 		write(1, " ", 1);
 		i++;
 	}
+}
+
+void	print_string(t_builtins *b, int start, int end) //check this
+{
+	while (start < end)
+	{
+		write(1, &b->value[start], 1);
+		start++;
+	}
+}
+
+void	handle_dolar(t_builtins *b)
+{
+	
 }
