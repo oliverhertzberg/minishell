451a247b12bdd79013c60663228766b79fe102fe
Simos Tigkas <stigkas@c1r5p16.hive.fi>
2024-04-30 14:42:33 +0300

    adding utils for eport

--- src/builtins/export_utils.c
+++ src/builtins/export_utils.c
@@ -1,25 +1,25 @@
 #include "../../headers/builtins.h"
 
-//use ft_strjoin
 void	append_value(t_hmap **v, char *key, char *value)
 {
 	t_hmap	*node;
-	char	*new_value;
+	char	*old_value;
 
 	node = *v;
-	new_value = (char *)malloc(ft_strlen(value) + 1);
 	while (node->next)
 	{
 		if (node->key == key)
 		{
+			old_value = (char *)malloc(ft_strlen(node->value) + 1);
+			old_value = node->value;
 			free(node->value);
-			node->value = (char *)malloc(ft_strlen(value) + 1);
+			node->value = (char *)malloc(ft_strlen(value) + ft_strlen(old_value) + 1);
 			if (!node->value)
 			{
 				free(node->value);
 				return ;
 			}
-			node->value = value;
+			node->value = ft_strjoin(old_value, value);
 			break ;
 		}
 		node = node->next;
