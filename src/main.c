605e23a3448a63138432695d8c806908aa7db0f6
Oliver Hertzberg <ohertzbe@c3r3p6.hive.fi>
2024-05-02 17:28:58 +0300

    add function to intialize variables in struct

--- src/main.c
+++ src/main.c
@@ -2,7 +2,24 @@
 
 void	initialize_t_parser(t_parser **p)
 {
-	
+	t_parser *current;
+	int num_of_cmds;
+
+	num_of_cmds = 0;
+	current = *p;
+	while (current)
+	{
+		current->fd_here_doc = -2;
+		current->fd_in = -2;
+		current->fd_out = -2;
+		current->infile = NULL;
+		current->outfile = NULL;
+		current->cmd_path = NULL;
+		current->args = NULL;
+		num_of_cmds++;
+		current = current->next;
+	}
+	(*p)->num_of_cmds = num_of_cmds;
 }
 
 int main(int argc, char **argv, char **env)
@@ -20,7 +37,7 @@ int main(int argc, char **argv, char **env)
 		input = readline("Minishell:$ ");
 		ft_strip(&input); // removes spaces before and after input
 		split_by_pipe(&p, input); // split input by pipes into separate strings
-		initialize_t_parser();
+		initialize_t_parser(&p);
 		parse_string(&p); // go through each string, and get necessary variables for command table
 		// cleaning strings based on quotes and spaces
 		// taking informations or printing errors if needed and freeing everything
