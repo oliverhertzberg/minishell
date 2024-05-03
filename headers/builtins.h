4ccfad9a6ccaaa1442c3277c1b2d23b294379be0
Tanja Menkovic <tmenkovi@c1r4p10.hive.fi>
2024-05-03 12:03:11 +0300

    organizing header

--- headers/builtins.h
+++ headers/builtins.h
@@ -22,21 +22,53 @@ typedef struct s_builtins
 	t_hmap	*h; // check should we sent it a separate arg or include it here
 }	t_builtins;
 
-int		key_exists(t_hmap *v, char *key);
-char	*take_key(char *input, char *i);
-char	*take_value(char *input, int *i);
-void	add_new_var(t_hmap **v, char *key, char *value);
-void	remove_var(t_hmap **v, char *key);
-void	change_value(t_hmap **v, char *key, char *value);
+/* exit.c */
+void ft_exit(t_hmap **env, char *input);
+
+/* pwd.c */
+char	*ft_pwd(void);
+
+/* cd.c */
+void ft_cd(char *input, t_env **env);
+
+/* env.c */
+t_hmap	*init_hmap(char **env);
+void	ft_env(t_hmap **hashmap, int is_env);
+
+/* echo.c */
+void	ft_echo(t_builtins *b);
+
+/* echo_utils.c */
+void	print_space(t_builtins *b, int i);
+void	print_string(t_builtins *b, int start, int end);
+void	handle_dolar(t_builtins *b, int start, int end);
+void	handle_mix(t_builtins *b, int start, int end);
+
+/* export.c */
+void	ft_export(char *input, t_builtins *b, int *i, t_hmap **hsmap);
+
+/* export_utils.c */
 void	append_value(t_hmap **v, char *key, char *value);
 void	check_append(t_hmap **hmap, char *key, char *value);
-void	ft_env(t_hmap **hashmap, int is_env);
-void 	ft_exit(t_hmap **env, char *input);
-void	ft_cd(char *input, t_hmap **env);
-char	*ft_pwd(void);
-void	ft_echo(char *input);
-t_hmap	*init_hmap(char **env);
-void	ft_export(char *input, t_builtins *b, int *i);
+
+/* unset.c */
 void	ft_unset(char *input, t_hmap **v);
+
+/* builtins.c */
+void  do_builtins(char *line, int *i, t_builtins *b);
+t_builtins  create_builtins(char **line);
+
+/* builtin_utils.c */
+void	update_dir(char *case, char *value, t_env **env);
+int	already_in_list(char *name, t_env *env);
+
+/* var_utils.c */ //split into 2 files!!!
+char	*take_key(char *input, char *i);
+char	*take_value(char *input, int *i);
+int	key_exists(t_hmap *v, char *ekey);
+char	*return_value_hash(t_hmsp *v, char *key);
+void	add_new_var(t_hmap **v, char *akey, char *avalue);
+void	remove_var(t_hmap **v, char *rkey);
+void	change_value(t_hmap **v, char *key, char *value);
 
 #endif
