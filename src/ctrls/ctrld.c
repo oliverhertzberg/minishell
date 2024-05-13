a5797e1c433d753731c8eae4657b69961ee3eb53
Safa Tuna <56443802+tunasafa@users.noreply.github.com>
2024-05-13 16:24:41 +0300

    Update ctrld.c

--- src/ctrls/ctrld.c
+++ src/ctrls/ctrld.c
@@ -11,3 +11,27 @@ typedef struct {
     struct termios termio1;
     struct termios termio2;
 } t_data;
+
+void set_signal_handlers(int mode, t_hmap **hmap)
+{
+	tcgetattr(STDIN_FILENO, &(t_data->termio1));
+	t_data->termio2 = t_data->termio1;
+	t_data->termio2.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &(t_data->termio2));
+	if (mode == 0)
+	{
+		signal(SIGINT, sigint_handler);
+		signal(SIGQUIT, SIG_IGN);
+
+	}
+	else if (mode == 1)
+	{
+		signal(SIGINT, sigint_handler);
+		signal(SIGQUIT, SIG_IGN);
+	}
+	else if (mode == 2)
+	{
+		signal(SIGINT, sigquit_handler);
+		signal(SIGQUIT, sigquit_handler);
+	}
+}
