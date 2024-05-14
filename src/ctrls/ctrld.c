8b61347d28bbd458e9137d439b5f913b257a4da6
Safa Tuna <56443802+tunasafa@users.noreply.github.com>
2024-05-14 17:20:08 +0300

    Update ctrld.c

--- src/ctrls/ctrld.c
+++ src/ctrls/ctrld.c
@@ -27,7 +27,7 @@ void sigint_handler(int signum)
 	(void)signum;
 	if(signum == SIGINT)
 	{
-		printf("\n");
+		write(STDOUT_FILENO, "\n", 1);
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
