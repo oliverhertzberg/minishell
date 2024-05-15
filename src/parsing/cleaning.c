6c5254afdd916223fef1ae3708ad41128eb9eec8
Tanja Menkovic <tmenkovi@c1r6p17.hive.fi>
2024-05-15 11:22:25 +0300

    quotes

--- src/parsing/cleaning.c
+++ src/parsing/cleaning.c
@@ -9,7 +9,10 @@ static void clean_cmd(t_cmd_data **d)
         || inside_mix((*d)->args[0], 0, ft_strlen((*d)->args[0])) == 2)
         ft_skip_fl(&((*d)->args[0]));
 }
-//not done
+/*
+error if quote is not closed
+cleaning if entire string is inside of pair of quotes and memorising which quotes were there
+*/
 void    clean_quotes(t_cmd_data **d)
 {
     int i;
@@ -19,12 +22,14 @@ void    clean_quotes(t_cmd_data **d)
     while ((*d)->args[i])
     {
         ft_strip((*d)->args[i]);
+        //if open and not closed
         if (check_word((*d)->args[i], 0, ft_strlen((*d)->args[0])) == 0)
         {
             //error open quote
             exit(1);
         }
-        else if (check_word((*d)->args[i], 0, ft_strlen((*d)->args[0])) == 2) //skipping useless quotes
+        // if open and closed (we are cleaning only if entire string is inside of quotes)
+        else if (check_word((*d)->args[i], 0, ft_strlen((*d)->args[0])) == 2)
         {
             if (is_inside_quotes((*d)->args[i], 0, ft_strlen((*d)->args[0])) == 1)
             {
@@ -36,10 +41,21 @@ void    clean_quotes(t_cmd_data **d)
                 (*d)->quote[i] = 1;
                 ft_skip_fl(&((*d)->args[i]));
             }
-            else if (is_inside_quotes((*d)->args[i], 0, ft_strlen((*d)->args[0])) == 3)
-                ft_skip((*d)->args[i]);
         }
-        else if (check_word((*d)->args[i], 0, ft_strlen((*d)->args[0])) == )
+        // if inside of quotes we have different quotes
+        else if (check_word((*d)->args[i], 0, ft_strlen((*d)->args[0])) == 1)
+        {
+            if (is_inside_quotes((*d)->args[i], 0, ft_strlen((*d)->args[0])) == 1)
+            {
+                (*d)->quote[i] = 2;
+                ft_skip_fl(&((*d)->args[i]));
+            }
+            else if (is_inside_quotes((*d)->args[i], 0, ft_strlen((*d)->args[0])) == 2)
+            {
+                (*d)->quote[i] = 1;
+                ft_skip_fl(&((*d)->args[i]));
+            }
+        }
         i++;
     }
 }
