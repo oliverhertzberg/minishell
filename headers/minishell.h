fa06f855af266c4a3ce72d08c70103d845ad0fde
Tanja Menkovic <tmenkovi@c2r1p3.hive.fi>
2024-05-16 11:27:19 +0300

    norm

--- headers/minishell.h
+++ headers/minishell.h
@@ -18,10 +18,10 @@ typedef struct s_hmap t_hmap; // this shouldn't be needed because the whole head
 // linked list for infiles, outfiles and heredocs
 typedef struct s_file
 {
-	char *file;
-	int fd;
-	int append;
-	struct s_file *next;
+	char			*file;
+	int				fd;
+	int				append;
+	struct s_file	*next;
 } t_file;
 
 // in case we get something like this:
@@ -30,23 +30,23 @@ typedef struct s_file
 // with arg_lst we get a linked list of strings, that we .
 typedef struct s_arg_lst
 {
-	char	*arg;
-	struct s_arg_lst *next;
+	char				*arg;
+	struct s_arg_lst	*next;
 }	t_arg_lst;
 
 // this list contains variables for the child processes that will execute the commands
 typedef struct	s_cmd_data
 {
-	int		in_use;
-	int		is_here_doc;
-	t_file	*infile;
-	t_file	*outfile;
-	t_file	*heredoc;
-	char	*cmd_path;
-	t_arg_lst *arg_lst;
-	int		arg_count;
-	char 	**args;
-	int		*quote; //0 if no quotes, 1 if it was inside of single, 2 double
+	int			in_use;
+	int			is_here_doc;
+	t_file		*infile;
+	t_file		*outfile;
+	t_file		*heredoc;
+	char		*cmd_path;
+	t_arg_lst	*arg_lst;
+	int			arg_count;
+	char 		**args;
+	int			*quote; //0 if no quotes, 1 if it was inside of single, 2 double
 	struct s_cmd_data *next;
 }	t_cmd_data;
 
@@ -57,8 +57,8 @@ typedef struct s_cmd_env
 	int		num_of_cmds;
 	char	**paths;
 	int 	exit_code;
-	char **env_copy;
-	t_hmap **hashmap;
+	char	**env_copy;
+	t_hmap	**hashmap;
 }	t_cmd_env;
 
 typedef struct s_input
@@ -71,13 +71,13 @@ typedef struct s_input
 
 /* arg_lst.c */
 t_arg_lst	*arg_lstnew(char *arg);
-void    arg_lstadd_back(t_arg_lst **lst, t_arg_lst *new);
-void	arg_lstclear(t_arg_lst **lst, int free_arg);
+void    	arg_lstadd_back(t_arg_lst **lst, t_arg_lst *new);
+void		arg_lstclear(t_arg_lst **lst, int free_arg);
 
 /* file_list_utils.c*/
-t_file	*file_lstnew(char *file, int fd, int append);
-void    file_lstadd_back(t_file **lst, t_file *new);
-void	file_lstclear(t_file **lst);
+t_file		*file_lstnew(char *file, int fd, int append);
+void    	file_lstadd_back(t_file **lst, t_file *new);
+void		file_lstclear(t_file **lst);
 
 /* parser_utils.c */
 t_cmd_data	*lstnew(void);
@@ -85,21 +85,21 @@ void    	lstadd_back(t_cmd_data **lst, t_cmd_data *new);
 void		lstclear(t_cmd_data **lst);
 
 /* parser.c */
-void    parse_input(t_cmd_data **c, char *input, t_cmd_env *c_env);
+void    	parse_input(t_cmd_data **c, char *input, t_cmd_env *c_env);
 
 /* split.c */
 //void    split_by_pipe(t_cmd_data **p, char *input);
 
 /* cleaning.c */
-void    init_quote(t_cmd_data **d);
-void    clean_quotes(t_cmd_data **d);
+void    	init_quote(t_cmd_data **d);
+void    	clean_quotes(t_cmd_data **d);
 
 /* init_c_env.c */
-void init_c_env(t_cmd_env *c, char **env);
+void		init_c_env(t_cmd_env *c, char **env);
 
 /* EXECUTION */
 /*execute_commands.c*/
-void    execution(t_cmd_data **c, t_cmd_env *c_env);
+void    	execution(t_cmd_data **c, t_cmd_env *c_env);
 t_cmd_data *pop_node_in_use(t_cmd_data **lst);
 
 
