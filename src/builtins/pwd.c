5bbb466a93bdfdd58610df640aa17b40cc339103
Simos Tigkas <stigkas@c2r3p9.hive.fi>
2024-05-16 15:31:27 +0300

    made it void function

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -1,12 +1,14 @@
 #include "../../headers/builtins.h"
 
-char	*ft_pwd(void)
+//maybe it needs to change back to be char ft_pwd
+void	ft_pwd(void)
 {
 	char	*pwd;
 
 	pwd = getcwd(NULL, 0);
 	if (!pwd)
 		exit(EXIT_FAILURE); //handle it
-	printf("%s\n", pwd);
-	return (pwd);
+	else
+		printf("%s\n", pwd);
+	// return (pwd);
 }
