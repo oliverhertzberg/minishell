04124d0d0afdfcec0de56470cf035745b9acdbe8
Simos Tigkas <stigkas@c2r3p9.hive.fi>
2024-05-16 15:33:22 +0300

    most of them work

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -3,22 +3,22 @@
 //add arguments to functions when they are done!
 void  do_builtins(t_cmd_data *d, t_cmd_env e)
 {
-	t_hmap	*path;
+	// t_hmap	*path;
 
   //if (ft_strcmp(d->args[0], "echo") == 0)
     //ft_echo();
   if (ft_strcmp(d->args[0], "cd") == 0)
-    ft_cd();
+    ft_cd(d, e.hashmap);
   if (ft_strcmp(d->args[0], "pwd") == 0)
     ft_pwd();
   if (ft_strcmp(d->args[0], "export") == 0)
-    ft_export();
+    ft_export(d, *e.hashmap);
   if (ft_strcmp(d->args[0], "unset") == 0)
-    ft_unset();
+    ft_unset(d, *e.hashmap);
   if (ft_strcmp(d->args[0], "env") == 0)
-    ft_env();
+    ft_env(*e.hashmap, 1);
   if (ft_strcmp(d->args[0], "exit") == 0)
-    ft_exit();
+    ft_exit(&d, "0"); //second argument should be e.exit_code
 }
 
  void is_builting(t_cmd_data *data, t_cmd_env e) // test this
