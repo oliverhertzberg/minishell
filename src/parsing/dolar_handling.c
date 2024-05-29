2581a9c0cc889920c24792fc714933b259358a26
Tanja Menkovic <tmenkovi@c1r3p8.hive.fi>
2024-05-29 14:49:09 +0300

    dollar

--- src/parsing/dolar_handling.c
+++ src/parsing/dolar_handling.c
@@ -11,58 +11,137 @@ static char	*single_quotes(char *str, int *j)
 	new_str = ft_substr(str, start, *j - start);
 	return (new_str);
 }
-static void	helping_function(char **str, int *i, int *j, char **new_str)
-{
-	char	*temp;
+// static void	helping_function(char **str, int *i, int *j, char **new_str)
+// {
+// 	char	*temp;
 
-	(*j)++;
-	temp = single_quotes(str[*i], j);
-	*new_str = ft_strjoin_new(new_str, &temp);
-	free(temp);
-	(*j)++;
-}
+// 	(*j)++;
+// 	temp = single_quotes(str[*i], j);
+// 	*new_str = ft_strjoin_new(new_str, &temp);
+// 	free(temp);
+// 	(*j)++;
+// }
+// static int	help_conditions(char **str, int *i)
+// {
+// 	if (is_in_str(str[*i], '\'', 0, ft_strlen(str[*i])) == -1
+// 		&& is_in_str(str[*i], '"', 0, ft_strlen(str[*i])) == -1
+// 		&& is_in_str(str[*i], '$', 0, ft_strlen(str[*i])) == -1)
+// 		return (1);
+// 	return (0);
+// }
 
-void	clean_dolar(char **str, t_hmap  **h, int exit_code)
+// void	clean_dolar(char **str, t_hmap  **h, int exit_code)
+// {
+// 	int		i;
+// 	int		j;
+// 	char	*new_str;
+// 	char	*temp;
+
+// 	i = 0;
+// 	while (str[i] != NULL)
+// 	{
+// 		new_str = NULL;
+// 		temp = NULL;
+// 		if (help_conditions(str, &i) == 1)
+// 			i++;
+// 		else
+// 		{
+// 			j = 0;
+// 			while (str[i][j])
+// 			{
+// 				if (str[i][j] == '\'')
+// 					helping_function(str, &i, &j, &new_str);
+// 				else if (str[i][j] == '"')
+// 				{
+// 					j++;
+// 					temp = double_quotes(str[i], &j, h, exit_code);
+// 					new_str = ft_strjoin_new(&new_str, &temp);
+// 					free(temp);
+// 					j++;
+// 				}
+// 				else
+// 				{
+// 					temp = no_quotes(str[i], &j, h, exit_code);
+// 					new_str = ft_strjoin_new(&new_str, &temp);
+// 					free(temp);
+// 				}
+// 			}
+// 			free(str[i]);
+// 			str[i] = ft_strdup(new_str);
+// 			free(new_str);
+// 			i++;
+// 		}
+// 	}
+// }
+
+typedef struct s_dollar
 {
-	int		i;
-	int		j;
+	char	**str;
+	t_hmap	**h;
+	int		ec;
 	char	*new_str;
 	char	*temp;
+}	t_dollar;
+
+static int	help_conditions(t_dollar *d, int *i)
+{
+	if (is_in_str(d->str[*i], '\'', 0, ft_strlen(d->str[*i])) == -1
+		&& is_in_str(d->str[*i], '"', 0, ft_strlen(d->str[*i])) == -1
+		&& is_in_str(d->str[*i], '$', 0, ft_strlen(d->str[*i])) == -1)
+		return (1);
+	return (0);
+}
+
+static void	helping_function(t_dollar *d, int *i, int *j)
+{
+	if (d->str[*i][*j] == '\'')
+	{
+		(*j)++;
+		d->temp = single_quotes(d->str[*i], j);
+		d->new_str = ft_strjoin_new(&(d->new_str), &(d->temp));
+		free(d->temp);
+		(*j)++;
+	}
+	else if (d->str[*i][*j] == '"')
+	{
+		(*j)++;
+		d->temp = double_quotes(d->str[*i], j, d->h, d->ec);
+		d->new_str = ft_strjoin_new(&(d->new_str), &(d->temp));
+		free(d->temp);
+		(*j)++;
+	}
+	else
+	{
+		d->temp = no_quotes(d->str[*i], j, d->h, d->ec);
+		d->new_str = ft_strjoin_new(&(d->new_str), &(d->temp));
+		free(d->temp);
+	}
+}
+
+void	clean_dlr(char **str, t_hmap  **h, int exit_code)
+{
+	t_dollar	d;
+	int			i;
+	int			j;
 
 	i = 0;
-	while (str[i] != NULL)
+	d.str = str;
+	d.h = h;
+	d.ec = exit_code;
+	while (d.str[i] != NULL)
 	{
-		new_str = NULL;
-		temp = NULL;
-		if (is_in_str(str[i], '\'', 0, ft_strlen(str[i])) == -1
-			&& is_in_str(str[i], '"', 0, ft_strlen(str[i])) == -1
-			&& is_in_str(str[i], '$', 0, ft_strlen(str[i])) == -1)
-			i++;
+		d.new_str = NULL;
+		d.temp = NULL;
+		if (help_conditions(&d, &i) == 1)
+			(i)++;
 		else
 		{
 			j = 0;
 			while (str[i][j])
-			{
-				if (str[i][j] == '\'')
-					helping_function(str, &i, &j, &new_str);
-				else if (str[i][j] == '"')
-				{
-					j++;
-					temp = double_quotes(str[i], &j, h, exit_code);
-					new_str = ft_strjoin_new(&new_str, &temp);
-					free(temp);
-					j++;
-				}
-				else
-				{
-					temp = no_quotes(str[i], &j, h, exit_code);
-					new_str = ft_strjoin_new(&new_str, &temp);
-					free(temp);
-				}
-			}
-			free(str[i]);
-			str[i] = ft_strdup(new_str);
-			free(new_str);
+				helping_function(&d, &i, &j);
+			free(d.str[i]);
+			d.str[i] = ft_strdup(d.new_str);
+			free(d.new_str);
 			i++;
 		}
 	}
