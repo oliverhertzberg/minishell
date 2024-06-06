41e1c89e3d61717e0a685100a8bb508687d5a59f
Oliver Hertzberg <ohertzbe@c2r1p7.hive.fi>
2024-06-06 14:49:27 +0300

    add quotehandling and expansion to input and output

--- src/parsing/redirections.c
+++ src/parsing/redirections.c
@@ -3,11 +3,23 @@
 static void	input_redirection(t_cmd_data **c, char *input, int *i)
 {
 	char	*infile;
+	char	**clean_dlr_array;
 
-	get_word(&infile, c, input, i);
+	clean_dlr_array = (char **)malloc(sizeof(char *) * 2);
+	get_word(&clean_dlr_array[0], c, input, i);
 	if ((*c)->env_ptr->parsing_error == 1)
+	{
+		free (clean_dlr_array[0]);
+		free (clean_dlr_array);
 		return ;
-	clean_dlr(&infile, (*c)->env_ptr->hashmap, (*c)->env_ptr->exit_code);
+	}
+	clean_dlr_array[1] = NULL;
+	clean_dlr(clean_dlr_array, (*c)->env_ptr->hashmap, (*c)->env_ptr->exit_code);
+	infile = ft_strdup(clean_dlr_array[0]);
+	if (!infile)
+		error_exit(NULL, "Malloc failed\n", c, 1);
+	free (clean_dlr_array[0]);
+	free (clean_dlr_array);
 	file_lstadd_back(&((*c)->infile), file_lstnew(infile, -2, 0));
 	if ((*c)->is_here_doc == 1)
 		(*c)->is_here_doc = 0;
@@ -16,10 +28,24 @@ static void	input_redirection(t_cmd_data **c, char *input, int *i)
 static void	output_redirection(t_cmd_data **c, char *input, int *i, int append)
 {
 	char	*file;
+	char	**clean_dlr_array;
 
-	get_word(&file, c, input, i);
+
+	clean_dlr_array = (char **)malloc(sizeof(char *) * 2);
+	get_word(&clean_dlr_array[0], c, input, i);
 	if ((*c)->env_ptr->parsing_error == 1)
+	{
+		free (clean_dlr_array[0]);
+		free (clean_dlr_array);
 		return ;
+	}
+	clean_dlr_array[1] = NULL;
+	clean_dlr(clean_dlr_array, (*c)->env_ptr->hashmap, (*c)->env_ptr->exit_code);
+	file = ft_strdup(clean_dlr_array[0]);
+	if (!file)
+		error_exit(NULL, "Malloc failed\n", c, 1);
+	free (clean_dlr_array[0]);
+	free (clean_dlr_array);
 	file_lstadd_back(&((*c)->outfile), file_lstnew(file, -2, append));
 }
 
