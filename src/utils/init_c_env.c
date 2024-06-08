29a10568d1a88a65cc83a24c689b947be8cd754e
Tanja Menkovic <menkovictanja@gmail.com>
2024-06-08 18:02:18 +0300

    pwd

--- src/utils/init_c_env.c
+++ src/utils/init_c_env.c
@@ -31,10 +31,11 @@ void	init_c_env(t_cmd_env *c, char **env)
 	c->pwd = ft_strdup(get_pwd(env));
 	if (!c->pwd)
 		malloc_error();
+	printf("init_c_env: %s\n", c->pwd); //test
 	add_shelllevel(c->hashmap);
-	// if (key_exists(c->hashmap, "PWD") == 0)
-	// {
-	// 	add_new_var(c->hashmap, "PWD", get_pwd(env));
-	// 	return ;
-	// }
+	if (key_exists(c->hashmap, "PWD") == 0)
+	{
+		add_new_var(c->hashmap, "PWD", get_pwd(env));
+		return ;
+	}
 }
